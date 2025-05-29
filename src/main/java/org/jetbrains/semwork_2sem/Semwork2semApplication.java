12a1dd67a8f5f69ed6c3875cc41201b22049301a
myrshe <polina.zimnyackova@gmail.com>
2025-05-29 15:20:58 +0300

    пустой проект

---
+++ src/main/java/org/jetbrains/semwork_2sem/Semwork2semApplication.java
@@ -0,0 +1,13 @@
+package org.jetbrains.semwork_2sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Semwork2semApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Semwork2semApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jetbrains/semwork_2sem/Semwork2semApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jetbrains.semwork_2sem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semwork2semApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
